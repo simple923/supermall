8ffd34fe8e70c7a426d23577183580d2fecbabe6
lile <2681195238@qq.com>
2022-08-08 16:42:14 +0800

    navbar封装与数据请求

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
-     <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/common/navbar/NavBar.vue'
+import {getHomeMultidata} from 'network/home'
   export default {
     name: "Home",
     components: {
+      NavBar
+    },
+    data(){
+      return{
+        banners:[],
+        recommends:[]
+      }
+    },
+    created(){
+        getHomeMultidata().then(res=>{
+        this.banners=res.data.banner.list;
+        this.recommends=res.data.recommend.list;
+
+        })
     }
   }
-</script>
+
+  </script>
 
 <style scoped>
+ .home-nav{
+  background-color: var(--color-tint);
+  color: #fff;
+ }
  
 </style>
